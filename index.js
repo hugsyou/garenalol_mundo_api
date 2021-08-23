877618b78f3d43a40323fc1fbd627e0105ce70a4
opart.y <opartyongnoo@gmail.com>
2021-08-23 11:37:34 +0700

    Add | Exporess 404

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const express = require('express');
 const app = express();
 app.use(express.json({ extended: false }));
 
+app.get('/',
+    function (req, res) {
+        res.status(200).json({ result: "ok" }).end();
+        return;
+    }
+);
+
+app.use(function (req, res, next) {
+    res.status(404).json({ result: "error", error: "ERROR_NOTFOUND" });
+    return;
+});
+
 const HTTP_PORT = process.env.PORT || 8080;
 app.listen(HTTP_PORT, () => console.log(`Server is running in port ${HTTP_PORT}`));
